87df1ba787446639bcf5c38f736077fccbcc8129
Yifu Yan <yanyifu94@hotmail.com>
2019-07-18 21:43:05 -0700

    added UI code to UI

--- ui.R
+++ ui.R
@@ -20,79 +20,19 @@ body <- dashboardBody(
                 column(
                     width = 6,
                     sidebarLayout(
-                        sidebarPanel(
-                            tags$h3("Plotting Matix:"),
-                            sliderInput(
-                                inputId = "D3MatrixPlotColumns",
-                                label = "Matrix Columns:",
-                                min = 1,
-                                max = 100,
-                                step = 1, 
-                                value = 5,
-                                round = TRUE
-                            ),
-                            sliderInput(
-                                inputId = "D3MatrixPlotRows",
-                                label = "Matrix Rows:",
-                                min = 1,
-                                max = 100,
-                                step = 1, 
-                                value = 5,
-                                round = TRUE
-                            ),
-                            tags$h3("Plotting Area:"),
-                            pickerInput(
-                                inputId = "D3MatrixPlotFix",
-                                label = "Fix SVG BY:",
-                                choices = c(
-                                    "width (use width provided by shiny)" = "width",
-                                    "height (use height provided by shiny)" = "height", 
-                                    "auto (use both width and height provided by shiny)" = "auto"
-                                ),
-                                selected = "width"
-                            ),
-                            numericInput(
-                                inputId = "D3MatrixPlotAspect",
-                                label = "Aspect Ratio(width/height)",
-                                value = 1,
-                                step = 1
-                            ),
-                            tags$h3("Colors:"),
-                            colourpicker::colourInput(
-                                inputId = "D3MatrixPlotBackground",
-                                label = "Pick Background Color:",
-                                value = "#ECF0F5"
-                            ),
-                            colourpicker::colourInput(
-                                inputId = "D3MatrixPlotRectFill",
-                                label = "Pick Rectangle Fill Color:",
-                                value = "#87ceeb"
-                            ),
-                            tags$h3("Margins:"),
-                            sliderInput(
-                                inputId = "D3MatrixPlotsvgMargin",
-                                label = "SVG Margin %",
-                                min = 0,
-                                max = 100,
-                                step = 1, 
-                                value = 5,
-                                post = "%"
-                            ),
-                            sliderInput(
-                                inputId = "D3MatrixPlotrectMargin",
-                                label = "Rectangle Margin %",
-                                min = 0,
-                                max = 100,
-                                step = 1, 
-                                value = 5,
-                                post = "%"
-                            )
-                        ),
+                        source("ui_codes/D3MatrixPlot.R",local = TRUE)$value,
                         mainPanel(
                             tabsetPanel(
                                 tabPanel(
                                     "Plot",
                                     d3Output("D3MatrixPlot1")
+                                ),
+                                tabPanel(
+                                    "UI Code",
+                                    tags$div(
+                                        style = "width:150%",
+                                        pre(includeText("ui_codes/D3MatrixPlot.R"))
+                                    )
                                 ),
                                 tabPanel(
                                     "Sever Code",
@@ -102,7 +42,7 @@ body <- dashboardBody(
                                     )
                                 ),
                                 tabPanel(
-                                    "Javascript Code", 
+                                    "D3 Code", 
                                     tags$div(
                                         style = "width:150%",
                                         pre(includeText("r2d3-snippets/D3MatrixPlot.js"))
